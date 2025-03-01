e2c882588618d2e95467c37c45023a00ba2eb23a
Nazrul Islam Rakib <88043438+TigerRakib@users.noreply.github.com>
2025-03-02 00:13:20 +0600

    Update settings.py

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qu8@8_m&_7s+(u3n)=h-y_q5$o1v3=-uwc_vnv+tn$^ceiyv+o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
